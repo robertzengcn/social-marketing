d44b3f792f930951c6c7ffc850ed300a32685390
robertzengcn <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>
2024-07-25 16:25:51 +0800

    fix command error

--- src/utilityCode.ts
+++ src/utilityCode.ts
@@ -2,18 +2,20 @@
 export {};
 import {Usersearchdata} from "@/entityTypes/searchControlType"
 import {UserSearch} from "@/modules/userSearch"
-
+import { argv } from 'node:process';
 
 async function runCommand(parameter) {
     //const action=process.argv[2]
+    console.log(parameter)
     const action=parameter[2]
+    console.log(action)
     switch(action){
         case "searchscraper":{
             const data=parameter[3]
             if(!parameter){
                 throw new Error("parameter is empty")
             }
-            const userSearchdata:Usersearchdata=JSON.parse(parameter)
+            const userSearchdata:Usersearchdata=JSON.parse(data)
             console.log(userSearchdata)
             const userSer=new UserSearch()
             await userSer.searchData(userSearchdata)
@@ -22,4 +24,5 @@ async function runCommand(parameter) {
         }
     }
 }
-runCommand(process.argv);
+//const input=process.argv
+runCommand(argv);
