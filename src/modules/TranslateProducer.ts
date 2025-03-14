9af002bf6f983869d865f5ad7ea366af50de1c8c
jianze_zeng <1093968009@qq.com>
2025-03-14 11:24:43 +0800

    update translate

--- src/modules/TranslateProducer.ts
+++ src/modules/TranslateProducer.ts
@@ -28,15 +28,15 @@ export class TranslateProducer {
                 throw new Error("llmconfig is required");
             }
 
-            return this.translateWithLLM(sourceLan.code,targetLan.code,text,llmconfig);
+            return this.translateWithLLM(sourceLan.code,targetLan.code,text,name,llmconfig);
         }else if(this.availableApi.includes(name)){
             //return this.translateWithAPI(name,sourceLan.code,targetLan.code,text,systemMsg);
         }
         return Promise.resolve(undefined);
     }
-    async translateWithLLM(inputlang:string,outputlang:string,text:string,llmconfig:LlmCongfig):Promise<string|void>{
+    async translateWithLLM(inputlang:string,outputlang:string,text:string,toolName:string,llmconfig:LlmCongfig):Promise<string|void>{
         let factory=new LlmFactory();
-        let tool:LlmImpl|undefined=factory.getLlmTool(llmconfig.model,llmconfig);
+        let tool:LlmImpl|undefined=factory.getLlmTool(toolName,llmconfig);
         console.log("tool",tool)
         if(tool){
             return tool.translate(inputlang,outputlang,text);
