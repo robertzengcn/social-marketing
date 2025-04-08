8ea7c8cab2feb5e5225a90af8a32eb679c218e3e
jianze_zeng <1093968009@qq.com>
2025-04-08 09:16:06 +0800

    add json-schema

--- forge.config.js
+++ forge.config.js
@@ -21,6 +21,7 @@ const EXTERNAL_DEPENDENCIES = [
 'js-tiktoken',
 'p-retry',
 'langsmith',
+'@cfworker/json-schema'
 ];
 //import { ForgeConfig } from '@electron-forge/shared-types';
 // import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
