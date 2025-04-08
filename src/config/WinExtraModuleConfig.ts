5d1939107dd0e216100c85ea64cb5e8b854d40e4
jianze_zeng <1093968009@qq.com>
2025-04-08 13:45:38 +0800

    fix version error

--- src/config/WinExtraModuleConfig.ts
+++ src/config/WinExtraModuleConfig.ts
@@ -5,7 +5,7 @@ export const WinExtraModuleConfig:ExtraModule[]=[
         name:"youtube-dl",
         // description:"video download is a module that can download video from tiktok, Instagram and other platforms",
         packagename:"yt-dlp.exe",
-        version:"2025.03.26",
+        version:"2025.03.31",
         link:"https://github.com/yt-dlp/yt-dlp/releases/download/2025.03.31/yt-dlp.exe",
         requirePy:false,
         ispip:false,
