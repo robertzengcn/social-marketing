251ec097b82fbe47b0f2b06d28f1563fbd0e65d2
robertzengcn <zengjianze@foxmail.com>
2025-11-16 10:37:30 +0800

    fix youtube download link error

--- src/config/WinExtraModuleConfig.ts
+++ src/config/WinExtraModuleConfig.ts
@@ -6,7 +6,7 @@ export const WinExtraModuleConfig:ExtraModule[]=[
         // description:"video download is a module that can download video from tiktok, Instagram and other platforms",
         packagename:"yt-dlp.exe",
         version:"2025.11.12",
-        link:"https://github.com/yt-dlp/yhttps://github.com/yt-dlp/yt-dlp/releases/download/2025.11.12/yt-dlp.exe",
+        link:"https://github.com/yt-dlp/yt-dlp/releases/download/2025.11.12/yt-dlp.exe",
         requirePy:false,
         ispip:false,
         requireFfmpeg:true
